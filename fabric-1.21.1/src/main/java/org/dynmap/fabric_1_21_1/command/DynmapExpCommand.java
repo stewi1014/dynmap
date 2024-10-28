6d88c69356766235e0acc1cfec81f18c679efd8f
Stewart Borle <stewi1014@gmail.com>
2024-10-28 19:57:41 +0800

    Update to fabric 1.21.1
    support for fabric-1.21.1

---
+++ fabric-1.21.1/src/main/java/org/dynmap/fabric_1_21_1/command/DynmapExpCommand.java
@@ -0,0 +1,9 @@
+package org.dynmap.fabric_1_21_1.command;
+
+import org.dynmap.fabric_1_21_1.DynmapPlugin;
+
+public class DynmapExpCommand extends DynmapCommandExecutor {
+    public DynmapExpCommand(DynmapPlugin p) {
+        super("dynmapexp", p);
+    }
+}
